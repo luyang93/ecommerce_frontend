58e09794c9e1996b297c5e716463a6180a342f47
luyang93 <510426762@qq.com>
2021-05-04 03:56:01 +0800

    Show a toast message

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -32,6 +32,7 @@
 
 <script>
 import axios from 'axios'
+import {toast} from 'bulma-toast'
 
 export default {
   name: "Product",
@@ -69,6 +70,15 @@ export default {
       }
 
       this.$store.commit('addToCart', item)
+
+      toast({
+        message: 'The product was added to the cart',
+        type: 'is-success',
+        dismissible: true,
+        pauseOnHover: true,
+        duration: 2000,
+        position: 'bottom-right',
+      })
     }
   }
 }
