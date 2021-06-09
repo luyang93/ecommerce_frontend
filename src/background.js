5ec4ea990b25b6a0a271f5c3bd5155f8b9e18317
luyang93 <510426762@qq.com>
2021-06-10 07:18:54 +0800

    webpack

--- src/background.js
+++ src/background.js
@@ -3,12 +3,52 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+
+const path = require("path")
+const url = require("url")
+const child = require("child_process")
+const MACOS = "darwin"
+const WINDOWS = "win32"
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
+
+
+var killStr = ""
+var appPath = path.join(app.getAppPath(), "backend", "manage.py" )
+var execPath = "python3"
+
+var log = require("electron-log")
+log.transports.console.level = true
+log.transports.file.level = true
+log.transports.file.level = "info"
+
+if ( process.platform == WINDOWS ) {
+  appPath = appPath.replace(/\\/g, "\\\\")
+  execPath = path.join(app.getAppPath(), "env", "bin", "python3" )
+} else if ( process.platform == MACOS ) {
+  var macAbsolutePath = path.join(app.getAppPath(), "env", "bin", "python3")
+  var env_path = macAbsolutePath+((process.env.PATH)?":"+process.env.PATH:"")
+  process.env.PATH = env_path
+  execPath = path.join(app.getAppPath(), "env", "bin", "python3" )
+} else {
+  log.error("Not on Windows or macOS?")
+  throw new Error("Not on Windows or macOS?")
+}
+
+log.info("Environment:\n", process.env)
+
+const childProcess = child.spawn(execPath, [appPath, "runserver", "0.0.0.0:8000")
+childProcess.stdout.on("data", (data) => {
+  log.info("stdout:\n", `${data}`)
+})
+childProcess.stderr.on("data", (data) => {
+  log.info("stderr:\n", `${data}`)
+})
 
 async function createWindow() {
   // Create the browser window.
@@ -47,7 +87,9 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (BrowserWindow.getAllWindows().length === 0) 
+    runBackend()
+    createWindow()
 })
 
 // This method will be called when Electron has finished
@@ -62,6 +104,7 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
+  runBackend()
   createWindow()
 })
 
